be5cc59a52a0866f5873863d2245496c632b97d3
jobson <jobsonoliveira@protonmail.com>
2023-09-26 05:52:46 -0300

    Tratamento de erro concluído.

--- Contador/src/Contador.java
+++ Contador/src/Contador.java
@@ -2,24 +2,22 @@ import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args) {
-        Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro: ");
-        int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro: ");
-        int parametroDois = terminal.nextInt();
-        contar(parametroUm, parametroDois);
 
-//        try {
-//            //chamando o método contendo a lógica de contagem
-//            contar(parametroUm, parametroDois);
-//
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
+        try {
+            Scanner terminal = new Scanner(System.in);
+            System.out.println("Digite o primeiro parâmetro: ");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro: ");
+            int parametroDois = terminal.nextInt();
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+
+        }
 
     }
 
-    static void contar(int parametroUm, int parametroDois) {
+    static void contar(int parametroUm, int parametroDois)  throws ParametrosInvalidosException {
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
         int contagem = parametroDois - parametroUm;
         if (parametroUm < parametroDois){
@@ -27,7 +25,7 @@ public class Contador {
                 System.out.println("Imprimindo o número " + i);
             }
         } else {
-            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            throw new ParametrosInvalidosException();
         }
 
     }
